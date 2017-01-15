7343f7406bdb40ba815b757af767d559ec7b8e7f
aronmorris <aron@undo.com>
2017-01-15 03:06:31 -0500

    yerboy

--- Next Prime Number/next_prime.rb
+++ Next Prime Number/next_prime.rb
@@ -1,3 +1,15 @@
+def get_primes
+  require 'prime'
+  next_prime = 0
+  while true
+    puts "'y' to continue, any to exit"
+    answer = gets.chomp
 
-class NextPrime
+    break unless answer == 'y'
+
+    puts Prime.lazy.select {|n| n > next_prime; next_prime = n if n > next_prime}.take(1).to_a
+
+  end
 end
+
+get_primes
